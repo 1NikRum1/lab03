ab64cc90b298f80b3468ac5bd1aa4cdd55bd6a1b
Nik <nek.55rumm@gmail.com>
2023-07-03 21:17:30 +0300

    доделал функцию с SVG

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -38,27 +38,34 @@ std::vector<size_t> make_histogram(const std::vector<double>& numbers, size_t bi
 
 void show_histogram_svg(const std::vector<size_t>& bins) {
     const auto IMAGE_WIDTH = 400;
-    const auto IMAGE_HEIGHT = 300;
-    const auto TEXT_LEFT = 20;
-    const auto TEXT_BASELINE = 20;
-    const auto TEXT_HEIGHT = 30;
-    const auto BIN_HEIGHT = IMAGE_HEIGHT - TEXT_HEIGHT;
+    const auto MAX_HEIGHT = 300.0;
     const auto BIN_WIDTH = IMAGE_WIDTH / bins.size();
+    const auto TEXT_HEIGHT = 20;
+    const auto TEXT_BASELINE = TEXT_HEIGHT;
     const std::vector<std::string> colors = { "#000", "#f00", "#0f0", "#00f", "#ff0", "#f0f", "#0ff" };
 
-    std::ofstream svg_file("histogram.svg");
-    svg_file << "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n";
-    svg_file << "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"" << IMAGE_WIDTH << "\" height=\"" << IMAGE_HEIGHT << "\">\n";
-
     size_t max_count = *std::max_element(bins.begin(), bins.end());
+    double scaling_factor = static_cast<double>(MAX_HEIGHT - TEXT_HEIGHT) / max_count;
+
+    std::ofstream svg_file("histogram.svg");
+    svg_file << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    svg_file << "<svg ";
+    svg_file << "width='" << IMAGE_WIDTH << "' ";
+    svg_file << "height='" << MAX_HEIGHT << "' ";
+    svg_file << "viewBox='0 0 " << IMAGE_WIDTH << " " << MAX_HEIGHT << "' ";
+    svg_file << "xmlns='http://www.w3.org/2000/svg'>\n";
+
     for (size_t i = 0; i < bins.size(); i++) {
-        size_t height = BIN_HEIGHT * (static_cast<double>(bins[i]) / max_count);
-        svg_file << "<rect x=\"" << i * BIN_WIDTH << "\" y=\"" << BIN_HEIGHT - height << "\" width=\"" << BIN_WIDTH << "\" height=\"" << height << "\" fill=\"" << colors[i % colors.size()] << "\" />\n";
+        double height = bins[i] * scaling_factor;
+        svg_file << "<rect x='" << i * BIN_WIDTH << "' y='" << MAX_HEIGHT - height << "' width='" << BIN_WIDTH << "' height='" << height << "' fill='" << colors[i % colors.size()] << "' />\n";
+        svg_file << "<text x='" << i * BIN_WIDTH + BIN_WIDTH / 2 << "' y='" << MAX_HEIGHT - height - TEXT_BASELINE / 2 << "'>" << bins[i] << "</text>\n";
     }
 
     svg_file << "</svg>\n";
     svg_file.close();
 }
+
+
 
 int main() {
     size_t number_count;
